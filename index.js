32c563fef91466ae8e9b4363bbf0f82fb4e412b6
19120694-19120670 <97723000+19120694-19120670@users.noreply.github.com>
2022-01-14 16:58:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World Trinh');
+  res.send('Hello Worlddd  Trinh');
 });
 
 var server = app.listen(3000, function () {
