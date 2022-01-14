d9019dd8cabc69b719fe846f5707c00279e2ad78
Châu Lý Phương Trinh <62369881+alibaba-boop-byte@users.noreply.github.com>
2022-01-14 11:01:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Trinh ne aaahihi');
+  res.send('Trinh ne aaaheeeeihi');
 });
 
 var server = app.listen(3000, function () {
